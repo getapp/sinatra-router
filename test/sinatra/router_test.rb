f55bf5650a80b75acc17f0a7f648b391d9755761
bcapdevi <bcapdevilacano@gmail.com>
2021-02-20 18:24:49 +0100

    Add test coverage when app.override_method

--- test/sinatra/router_test.rb
+++ test/sinatra/router_test.rb
@@ -13,6 +13,7 @@ Apps = (0..3).map do |i|
   Sinatra.new do
     set :raise_errors, true
     set :show_exceptions, false
+    enable :method_override
 
     get "/app#{i}" do
       headers['X-Cascade'] = 'pass' if params[:pass] == 'true'
@@ -20,6 +21,16 @@ Apps = (0..3).map do |i|
     end
   end
 end
+
+AppWithMethodOverride =
+  Sinatra.new do
+    set :method_override, true
+  end
+
+App =
+  Sinatra.new do
+    set :method_override, false
+  end
 
 # generates a condition lambda suitable for testing
 def condition(identifier)
@@ -108,7 +119,7 @@ describe Sinatra::Router do
           end
         end
 
-        run ->(_env) { [404, {}, []] }
+        run AppWithMethodOverride
       end
     end
 
@@ -155,6 +166,40 @@ describe Sinatra::Router do
       header 'X-Cond2', 'true'
       get '/app3'
       assert_equal 404, last_response.status
+    end
+
+    describe 'with method override' do
+      def app
+        Rack::Builder.new do
+          use Sinatra::Router do
+            mount Apps[0]
+          end
+
+          run AppWithMethodOverride
+        end
+      end
+
+      it 'converts the method to get' do
+        post '/app0', { _method: 'get' }
+        assert_equal 200, last_response.status
+      end
+    end
+
+    describe 'without method override' do
+      def app
+        Rack::Builder.new do
+          use Sinatra::Router do
+            mount Apps[0]
+          end
+
+          run App
+        end
+      end
+
+      it 'does not convert the method to get' do
+        post '/app0', { _method: 'get' }
+        assert_equal 404, last_response.status
+      end
     end
   end
 end
